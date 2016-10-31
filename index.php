df3cf4fca213ee8f0f517dfacc368f0c524530af
shaun <shaundonegan32@gmail.com>
2016-10-31 23:28:30 +0000

    update index

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
+<html>
+<body>
+<h1>CA377 Park@DCU</h1>
 
-echo "Hello world";
+<h2>"Real-time" availability</h2>
+<form action="webservice.php" method="get">
+Carpark: <input type="text" name="carpark"><br>
+<input type="submit">
+</form>
+<hr/>
 
-?>
+</body>
+</html>
