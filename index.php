68548cad5aaa9c6efe3a1017ae908127522d373f
ShaunJMD <shaundonegan32@gmail.com>
2016-11-03 19:23:14 +0000

    Update index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h1>Hello World</h1>
+<h1>Hello World aftermath</h1>
 <form action="test.php" method="get">
 Enter your Name: <input type="text" name="input"><br>
 <input type="submit">
